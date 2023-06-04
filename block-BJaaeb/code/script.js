13d1fce8ea9a6fe7d7d18e572c0e56031b440681
harish8930 <129581462+harish8930@users.noreply.github.com>
2023-06-04 12:01:27 +0530

    Update script.js

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -4,8 +4,24 @@ Write a function named `createInputElm` that accepts two parameter (label and `t
 default value to be "text" and return the input element inside label. (create it using `createElement`)
 
 */
+function createInputElm(label, type = "text") {
+  // Create the label element
+  const labelElement = document.createElement("label");
 
-// Your code goes here
+  // Create the input element
+  const inputElement = document.createElement("input");
+  inputElement.type = type;
+
+  // Set the label text
+  labelElement.textContent = label;
+
+  // Append the input element to the label element
+  labelElement.appendChild(inputElement);
+
+  // Return the label element with the input element inside
+  return labelElement;
+}
+
 
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
@@ -22,6 +38,25 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
+function createList(data) {
+  // Create the unordered list element
+  const ulElement = document.createElement("ul");
+
+  // Iterate over the data array
+  for (let i = 0; i < data.length; i++) {
+    // Create a list item element
+    const liElement = document.createElement("li");
+    
+    // Set the text content of the list item
+    liElement.textContent = data[i];
+    
+    // Append the list item to the unordered list
+    ulElement.appendChild(liElement);
+  }
+
+  // Return the generated HTML markup
+  return ulElement.outerHTML;
+}
 
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
